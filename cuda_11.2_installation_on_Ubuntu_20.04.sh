e0f82472af176874ed1f02e5b0b98f9ea1b18605
PhuongNDVN <34764026+PhuongNDVN@users.noreply.github.com>
2022-01-17 15:35:06 +0900

    Create cuda_11.2_installation_on_Ubuntu_20.04.sh
    Init

---
+++ cuda_11.2_installation_on_Ubuntu_20.04.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+### steps ####
+# verify the system has a cuda-capable gpu
+# download and install the nvidia cuda toolkit and cudnn
+# setup environmental variables
+# verify the installation
+###
+
+### to verify your gpu is cuda enable check
+lspci | grep -i nvidia
+
+### If you have previous installation remove it first. 
+sudo apt-get purge nvidia*
+sudo apt remove nvidia-*
+sudo rm /etc/apt/sources.list.d/cuda*
+sudo apt-get autoremove && sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*
+
+# remove all previous cuda version (var, etc, ...)
+#sudo find / -name cuda*
+
+
+# system update
+sudo apt-get update
+sudo apt-get upgrade
+
+# install other import packages
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+# first get the PPA repository driver
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+
+# install nvidia driver with dependencies
+sudo apt install libnvidia-common-470
+sudo apt install libnvidia-gl-470
+sudo apt install nvidia-driver-470
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+
+ # installing CUDA-11.2
+sudo apt install cuda-11-2 
+
+# setup your paths
+echo 'export PATH=/usr/local/cuda-11.2/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+sudo ldconfig
+
+# install cuDNN v11.2
+# For downloading cuDNN v11.2 you have to be regeistered here https://developer.nvidia.com/developer-program/signup
+
+CUDNN_TAR_FILE="cudnn-11.2-linux-x64-v8.1.1.33.tgz"
+wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.1.1.33/11.2_20210301/cudnn-11.2-linux-x64-v8.1.1.33.tgz
+tar -xzvf ${CUDNN_TAR_FILE}
+
+# copy the following files into the cuda toolkit directory.
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-11.2/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-11.2/lib64/
+sudo chmod a+r /usr/local/cuda-11.2/lib64/libcudnn*
+
+# Finally, to verify the installation, check
+nvidia-smi
+nvcc -V
